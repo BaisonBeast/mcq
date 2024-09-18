bda633a09bb69b2cda53d4089ac16f07942d37ba
Priyanshu suman <priyanshu@Priyanshus-MacBook-Air.local>
2024-09-18 15:41:19 +0530

    Mcq changes

--- index.js
+++ index.js
@@ -7,26 +7,31 @@ const mcqs = [
         question: "Is Paris the capital of France?",
         option1: "Yes",
         option2: "No",
+        answer: 'Yes'
     },
     {
         question: "Is Mars the largest planet in our solar system?",
         option1: "Yes",
         option2: "No",
+        answer: 'Yes'
     },
     {
         question: "Is the Pacific Ocean the largest ocean on Earth?",
         option1: "Yes",
         option2: "No",
+        answer: 'Yes'
     },
     {
         question: "Did William Shakespeare write 'Hamlet'?",
         option1: "Yes",
         option2: "No",
+        answer: 'No'
     },
     {
         question: "Is CO2 the chemical symbol for water?",
         option1: "Yes",
         option2: "No",
+        answer: 'No'
     },
 ];
 
