93dc8d2616db3d8c18dfa69918feba7cfa8c8c10
Priyanshu suman <priyanshu@Priyanshus-MacBook-Air.local>
2024-09-18 14:55:36 +0530

    Mcq changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-[
+const mcqs = [
     {
         question: "Is Paris the capital of France?",
         option1: "Yes",
